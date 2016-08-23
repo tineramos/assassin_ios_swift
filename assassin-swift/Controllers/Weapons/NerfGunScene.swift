a895697e92e82dec158bd5e6ae278be7f95b3c8c
Tine Ramos <christine.ramos@ostmodern.co.uk>
2016-08-23 09:41:26 +0100

    position and add material to nerf ball - golf ball

--- assassin-swift/Controllers/Weapons/NerfGunScene.swift
+++ assassin-swift/Controllers/Weapons/NerfGunScene.swift
@@ -12,7 +12,19 @@ class NerfGunScene: SCNScene {
 
     func display() {
         
+        let geometry = SCNSphere(radius: 1.5)
+        geometry.firstMaterial?.diffuse.contents = UIColor.whiteColor()
+        geometry.firstMaterial?.diffuse.wrapS = .ClampToBorder
+        geometry.firstMaterial?.reflective.contents = "ball_texture.jpg"
+        geometry.firstMaterial?.fresnelExponent = 1.3
         
+        let sphere = SCNNode(geometry: geometry)
+        sphere.position = SCNVector3(x: 0, y: 0, z: 0)
+        
+//        sphere.physicsBody = SCNPhysicsBody.dynamicBody()
+//        sphere.physicsBody!.restitution = 0.9
+        
+        rootNode.addChildNode(sphere)
         
     }
     
